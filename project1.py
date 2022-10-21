fee199e099777a84c288ad9054baf241daada075
MD Golam Rabbi <rabbi.kaunia@gmail.com>
2022-10-21 19:30:13 +0600

    first commit

---
+++ project1.py
@@ -0,0 +1,14 @@
+# # students_count = 1000
+# # rating = 4.09
+# # is_published = True
+# # couse_name = "Python Programming"
+
+
+# from unicodedata import name
+
+
+# name 
+
+
+name = input ("What is your name? ")
+print ("Hi " + name)
