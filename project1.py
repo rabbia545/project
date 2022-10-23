c486b931712d5a2272dc5dca58aa28a40601618c
MD Golam Rabbi <rabbi.kaunia@gmail.com>
2022-10-23 06:14:05 +0600

    done

--- project1.py
+++ project1.py
@@ -1,60 +1,65 @@
 
-# Variable
-from ensurepip import version
+# # Variable
+# from ensurepip import version
 
 
-name = input ("What is your name? ")
-print ("Hi " + name )
-age = input ("How old are you? ")
-print ("My age is : " + age )
-study = input ("Which class do you Reading in? ")
-print ("I'm Reading in " + study )
+# name = input ("What is your name? ")
+# print ("Hi " + name )
+# age = input ("How old are you? ")
+# print ("I am  " + age )
+# study = input ("Which class do you Reading in? ")
+# print ("I'm Reading in " + study )
 
 
-# if else 
-a = input ("Enter your Bangla Marks : \n")
-b = input ("Enter your English Marks : \n")
-c = input ("Enter your Math Marks : \n")
-d = input ("Enter your Socal Science Marks : \n")
-e = input ("Enter your Physics Marks : \n")
-a = int (a)
-b = int (b)
-c = int (c)
-d = int (d)
-e = int (e)
-persentage = (a+b+c+d+e)*(100/400)
-print (persentage)
+# # if else 
+# a = input ("Enter your Bangla Marks : \n")
+# b = input ("Enter your English Marks : \n")
+# c = input ("Enter your Math Marks : \n")
+# d = input ("Enter your Socal Science Marks : \n")
+# e = input ("Enter your Physics Marks : \n")
+# a = int (a)
+# b = int (b)
+# c = int (c)
+# d = int (d)
+# e = int (e)
+# persentage = (a+b+c+d+e)*(100/480)
+# print (persentage)
 
 
 
-# from msilib.schema import Error
+# # from msilib.schema import Error
 
 
-Bangla = 55
-# if Bangla >= 90:
-#     print ("Pass")
-if Bangla >= 80:
-    print("A+")
-elif Bangla >=70:
-    print ("A")
-elif Bangla >=60:
-    print ("A-")
-elif Bangla >=50:
-    print ("B")
-elif Bangla >=40:
-    print ("C")
-elif Bangla >=33:
-    print ("D")
-else:
-    print("Fail")
-print ("Done")
+# Bangla = 55
+# # if Bangla >= 90:
+# #     print ("Pass")
+# if Bangla >= 80:
+#     print("A+")
+# elif Bangla >=70:
+#     print ("A")
+# elif Bangla >=60:
+#     print ("A-")
+# elif Bangla >=50:
+#     print ("B")
+# elif Bangla >=40:
+#     print ("C")
+# elif Bangla >=33:
+#     print ("D")
+# else:
+#     print("Fail")
+# print ("Done")
 
 
-for loop in range (5,25,2):
-    print(loop)
-for i in range(1,101):
-    print(i)
-
-python -version
+# for loop in range (5,25,2):
+#     print(loop)
+# for i in range(1,101):
+#     print(i)
 
 
+
+# for i in range (2,10,2):
+#     print (i)
+for s in range (1,51,5):
+    print (s)
+a = 9
+b = 4
